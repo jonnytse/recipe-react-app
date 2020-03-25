15e49b93ab2ad774a358bfe4475ac417565440e9
Jonny Tse <Jonny.Tse@Nology.local>
2020-03-25 23:45:45 +0000

    for now, added line 3 into Routes.jsx to remove Redirect and globalHistory

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Router, Redirect, globalHistory } from "@reach/router";
+// import { Router, Redirect, globalHistory } from "@reach/router";
+import { Router } from "@reach/router";
 import App from '../container/App';
 import Recipe from '../components/Recipe';
 class Routes extends Component {
