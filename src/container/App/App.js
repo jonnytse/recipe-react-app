145563e8f0262eb63c1b0dae0a4d5cbcd6a493a1
Jonny Tse <Jonny.Tse@Nology.local>
2020-03-25 20:08:18 +0000

    increased rendered items from 10 to 99

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -46,12 +46,12 @@ class App extends Component {
   grabRecipe = async (event) => {
     const recipeName = event.target.elements.recipeName.value;
     event.preventDefault();
-    const exampleRequest = await fetch(`https://api.edamam.com/search?q=${recipeName}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=10&calories=591-722&health=alcohol-free`); 
+    const exampleRequest = await fetch(`https://api.edamam.com/search?q=${recipeName}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=99&calories=591-722&health=alcohol-free`); 
     console.log(recipeName);
 
     const data = await exampleRequest.json();
     console.log(data);
-    console.log(data.hits);
+    // console.log(data.hits);
     // console.log(data.hits[0].recipe.label);
     this.setState({ recipes: data.hits });
     console.log(this.state.recipes);
